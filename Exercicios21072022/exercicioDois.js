6301f7602cdf6fd08ec6d42400071809aa8ba320
cmaas <clio.maas@mercadolivre.com>
2022-07-25 11:09:28 -0300

    reducing logic on exercises

--- Exercicios21072022/exercicioDois.js
+++ Exercicios21072022/exercicioDois.js
@@ -1,43 +1,17 @@
 let products = require('./products')
 
-function addProduct(array, name, price, quantity, colors){
+const addProduct = (array, name, price, quantity, colors) => {
     newProduct = {name: name, price: price, quantity:quantity, colors:colors}
     array.push(newProduct)
 }
 addProduct(products,"VideoGame", 4500, 50, ["black", "white"])
 
-function removeOutOfStock(array){
-    for(i = 0; i < array.length; i++){
-        if(!array[i].quantity){
-            array.splice(i, 1)
-        }
-}
-}
+const removeOutOfStock = (array) => array.forEach((a, i) => a.quantity == 0 ? array.splice(i, 1) : true)
 removeOutOfStock(products)
+console.log(products)
 
-function sumQuantity(array){
-    let sum = 0
-    for(i = 0; i < array.length; i++){
-        sum += array[i].quantity;
-    }
-    console.log(sum)
-}
-sumQuantity(products)
+const printValue = (array, price) => array.forEach((a) => a.price > price && console.log(a))
+printValue(products, 900)
 
-function printValue(array, price){
-    for(i = 0; i < array.length; i++){
-        if(array[i].price > price){
-            console.log(array[i])
-        }
-    }
-}
-printValue(products,900)
-
-function printNameLetter(array, letter){
-    for(i = 0; i < array.length; i++){
-        if(array[i].name.includes(letter)){
-            console.log(array[i])
-        }
-    }
-}
-printNameLetter(products, 'o')
+const printNameLetter = (array, letter) => console.log(array.filter((a) => a.name.includes(letter)))
+printNameLetter(products, 'y')
--- Exercicios21072022/exercicioUm.js
+++ Exercicios21072022/exercicioUm.js
@@ -1,48 +1,21 @@
 let products = require('./products')
 
 
-function addId(array){
-    for(i = 0; i < array.length; i++){
-        array[i].id = i + 1;
-    }
-}
+const addId = (array) => array.forEach((a, i) => a.id = i + 1)
 addId(products);
 console.log(products)
 
-function printName(array){
-    for(i = 0; i < array.length; i++){
-        console.log(array[i].name);
-    }
-}
+const printName = (array) => array.forEach((a) => console.log(a.name))
 printName(products)
 
-function printId(array, id){
-    for(i = 0; i < array.length; i++){
-        if(array[i].id == id){
-            console.log(array[i])
-        }
-    }
-}
-printId(products,3)
+const printId = (array, id) => console.log(array.find((a) => a.id === id))
+printId(products, 3)
 
-function printColor(array, color){
-    let productsFiltered = []
-    for(i = 0; i < array.length; i++){
-        if(array[i].colors.find(element => element == color)){
-            productsFiltered.push(array[i])
-        }
-    }
-    console.log(productsFiltered)
+const printColor = (array, color) => {
+    const filtered = array.filter((a) => a.colors.includes(color))
+    console.log(filtered)
 }
-printColor(products,"black")
+printColor(products, 'black')
 
-function printNoColor(array){
-    let productsFiltered = []
-    for(i = 0; i < array.length; i++){
-        if(array[i].colors.length === 0){
-            productsFiltered.push(array[i])
-        }
-    }
-    console.log(productsFiltered)
-}
+const printNoColor = (array) => console.log(array.filter((a) => a.colors.length == 0))
 printNoColor(products)
